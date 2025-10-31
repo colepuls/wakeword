74640e31199c45d397a7646f78c33defc266754b
Cole Puls <colepuls@me.com>
2025-10-31 14:56:53 -0500

    Split data arrays into train and val with a 80/20 split.

--- data_scripts/split_data.py
+++ data_scripts/split_data.py
@@ -26,6 +26,8 @@ BACKGROUND_TRAIN_PATH = os.getenv("BACKGROUND_TRAIN_PATH")
 WAKEWORD_VAL_PATH = os.getenv("WAKEWORD_VAL_PATH")
 BACKGROUND_VAL_PATH = os.getenv("BACKGROUND_VAL_PATH")
 
+VAL_SPLIT = 0.2
+
 def load_data(path) -> list:
     files = glob.glob(f"{path}*.wav")
     data = []
@@ -34,14 +36,32 @@ def load_data(path) -> list:
         waveform, sr = sf.read(f)
         data.append((waveform, sr))
 
-    print(len(data))
-    print(type(data))
-
     return data
 
-# def split_data():
+def split_data():
+    # Get raw data
+    raw_wakeword_data = load_data(WAKEWORD_DATA_PATH)
+    raw_background_data = load_data(OUTSIDE_DATA_PATH)
+
+    # Get val data
+    val_wakeword_split = int(len(raw_wakeword_data) * VAL_SPLIT)
+    val_wakeword_data = raw_wakeword_data[:val_wakeword_split]
+
+    val_background_split = int(len(raw_background_data) * VAL_SPLIT)
+    val_background_data = raw_background_data[:val_background_split]
+
+    # Get train data
+    train_wakeword_data = raw_wakeword_data[val_wakeword_split:]
+    train_background_data = raw_background_data[val_background_split:]
+
+    # Print val lengths
+    print(f"Val wakeword data length: {len(val_wakeword_data)}\n")
+    print(f"Val background data length: {len(val_background_data)}\n")
+
+    # Print train lengths
+    print(f"Train wakeword data length: {len(train_wakeword_data)}\n")
+    print(f"Train background data length: {len(train_background_data)}\n")
+    
 
 if __name__ == '__main__':
-    load_data(WAKEWORD_DATA_PATH)
-    load_data(OUTSIDE_DATA_PATH)
-
+    split_data()
